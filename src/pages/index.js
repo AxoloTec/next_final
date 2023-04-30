880e8541cd6197cd08fe895902298f25d65fb86a
Eva Perez <55931453+EvaStilinski@users.noreply.github.com>
2023-04-29 23:07:59 -0600

    html pages in public(not functional yet)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Menu from "../components/Menu";
 import Welcome from "../components/Welcome";
+import Metrics from "../components/Metrics";
 
 
 function Home() {
   return (
     <div className="home-container">
-      <Menu />
-      <div className="pagina">
 
-        <Welcome />
+      <div className="item-container">
+        <Menu />
+        <Metrics />
 
       </div>
     </div>
